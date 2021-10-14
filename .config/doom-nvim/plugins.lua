d91a74e0528bb827898c03ba8f84714601398a5b
Pierce Bartine <piercebartine@gmail.com>
2021-10-14 00:37:45 -0700

    Add doom-nvim initial config

---
+++ .config/doom-nvim/plugins.lua
@@ -0,0 +1,19 @@
+-- plugins - Doom nvim custom plugins
+--
+-- This file contains all the custom plugins that are not in Doom nvim but that
+-- the user requires. All the available fields can be found here
+-- https://github.com/wbthomason/packer.nvim#specifying-plugins
+--
+-- By example, for including a plugin with a dependency on telescope:
+-- return {
+--     {
+--         'user/repository',
+--         requires = { 'nvim-lua/telescope.nvim' },
+--     },
+-- }
+
+return {
+    {
+        'lifepillar/vim-solarized8'
+    },
+}
