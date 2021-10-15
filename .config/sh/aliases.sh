4a24fb3a7fb62247b79d521a03bae9e35b2ea1ac
Pierce Bartine <piercebartine@gmail.com>
2021-10-14 20:43:58 -0700

    Switch vi/vim alii back to nvim

--- .config/sh/aliases.sh
+++ .config/sh/aliases.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-alias vi="lvim"
-alias vim="lvim"
+alias vi="nvim"
+alias vim="nvim"
 alias cat="bat"
 alias ls="exa"
 alias l="exa --all --long --git --header"
 alias aliasrc="$EDITOR ${XDG_CONFIG_HOME}/sh/aliases.sh"
 alias zshrc="${EDITOR} ${ZDOTDIR}/.zshrc"
-
+alias copy="pbcopy"
