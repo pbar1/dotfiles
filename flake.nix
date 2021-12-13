c4b39c3525caa91948460b3f454f40144d3da548
Pierce Bartine <piercebartine@gmail.com>
2021-12-13 13:24:38 -0600

    Proper name

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "Pierce's Nix config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, darwin, nixpkgs }: {
-    darwinConfigurations."pbartine-ltm" = darwin.lib.darwinSystem {
+    darwinConfigurations."pbartine-ltm.internal.salesforce.com" = darwin.lib.darwinSystem {
       system = "x86_64-darwin";
       modules = [ ./macos ];
     };
