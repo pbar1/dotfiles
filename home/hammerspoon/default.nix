a52613a604103646dc526bb25feb94c788437be5
Pierce Bartine <piercebartine@gmail.com>
2024-01-15 14:22:40 -0800

    [home] Add Hammerspoon todos

--- home/hammerspoon/default.nix
+++ home/hammerspoon/default.nix
@@ -1,9 +1,10 @@
 { pkgs, lib, ... }:
 
+# FIXME: Only activate on macOS
 {
   home.file.".hammerspoon/init.lua".source = ./init.lua;
 
-  # TODO: Resolve Spoons packages dynamically
+  # TODO: Resolve Spoon packages dynamically
   home.file.".hammerspoon/Spoons/Caffeine.spoon".source = pkgs.myHammerspoonPlugins.Caffeine.src;
   home.file.".hammerspoon/Spoons/KSheet.spoon".source = pkgs.myHammerspoonPlugins.KSheet.src;
   home.file.".hammerspoon/Spoons/ReloadConfiguration.spoon".source = pkgs.myHammerspoonPlugins.ReloadConfiguration.src;
