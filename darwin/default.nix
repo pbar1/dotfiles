ccf27481d994b9083bf8818d6a6bcdada8880be1
Pierce Bartine <piercebartine@gmail.com>
2024-11-22 11:20:08 -0800

    [darwin] Add devenv cache

--- darwin/default.nix
+++ darwin/default.nix
@@ -19,10 +19,12 @@
   '';
   nix.settings.substituters = [
     "https://nix-community.cachix.org"
+    "https://devenv.cachix.org"
     "https://pbar1.cachix.org"
   ];
   nix.settings.trusted-public-keys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
     "pbar1.cachix.org-1:DsBqAi4CnR7TaABRn59sUBBK+lofYhQaV8lK8nl2gow="
   ];
   nix.distributedBuilds = true;
