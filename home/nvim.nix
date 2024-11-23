145d3283d26167c7377fd6e23b45d3a4ed9cee2e
Pierce Bartine <piercebartine@gmail.com>
2024-11-23 10:45:30 -0800

    [home] Add nvim telescope, which-key

--- home/nvim.nix
+++ home/nvim.nix
@@ -5,5 +5,8 @@
     enable = true;
     colorschemes.gruvbox.enable = true;
     plugins.lualine.enable = true;
+    plugins.telescope.enable = true;
+    plugins.which-key.enable = true;
+    plugins.web-devicons.enable = true; # required for telescope
   };
 }
