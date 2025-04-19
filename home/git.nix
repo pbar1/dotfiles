7191dbbe3750a0df121ece0cc09986e05ba6d6f6
Pierce Bartine <piercebartine@gmail.com>
2025-04-18 23:19:56 -0700

    nvim

--- home/git.nix
+++ home/git.nix
@@ -56,6 +56,7 @@ in
     ignores = [
       "**/.idea/*"
       "**/.DS_Store"
+      "**/Session.vim"
     ];
   };
 
--- home/nvim.nix
+++ home/nvim.nix
@@ -3,11 +3,34 @@
 {
   programs.nixvim = {
     enable = true;
+
+    clipboard.register = "unnamedplus"; # yank to system clipboard
+    globals.mapleader = " ";
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>ff";
+        action = "<cmd>Telescope find_files<cr>";
+        options.desc = "Find files";
+      }
+    ];
+
     colorschemes.gruvbox.enable = true;
-    plugins.lualine.enable = true;
-    plugins.telescope.enable = true;
-    plugins.which-key.enable = true;
+
     plugins.web-devicons.enable = true; # required for telescope
-    plugins.dashboard.enable = true;
+
+    plugins.mini.enable = true;
+    plugins.mini.modules = {
+      starter = { };
+      sessions = { };
+    };
+
+    plugins.lualine.enable = true;
+
+    plugins.telescope.enable = true;
+
+    plugins.which-key.enable = true;
+
+    plugins.blink-cmp.enable = true;
   };
 }
